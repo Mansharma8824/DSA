fae9b64a4cfc7c29516dfaa79a08e0b4822af3be
Manish Kumar <manishsharma9675800189@gmail.com>
2026-01-02 22:13:49 +0530

    jc

--- Data Structure/Linked List DS/Basic_LL.py
+++ Data Structure/Linked List DS/Basic_LL.py
@@ -1,6 +1,6 @@
 # Here we will understand the basic of linked list data structure
 
-# creating Node 
+# creating Nodes 
 class Node:
     def __init__(self,data):
         self.data = data
@@ -10,13 +10,27 @@ node1 = Node(3)
 node2 = Node(6)
 node3 = Node(8)
 
+# linking the nodes in linked list or creating linked list
 node1.next = node2
 node2.next = node3
+
+# making the node1 as head 
 head = node1
 
-current = head
+# traversaling in the linked list
+def PrintLinkedList(head):
+    current = head
+    while current != None:
+        print(current.data, end = " ")
+        current = current.next
+  
+#  creating the new node at the begning of the linked list
+new_node = Node(2)
+new_node.next = head
+head = new_node
 
-while current != None:
-    print(current.data, end = " ")
-    current = current.next
+
+PrintLinkedList(head)
+    
+
 
